604a65161675683b4b6af0542b5fc193feadbd61
Darius Artem Atlas <31390399+darius-atlas@users.noreply.github.com>
2021-07-02 19:08:20 -0700

    add player migration

---
+++ database/migrations/2021_07_03_020709_create_players_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('answers')->default(0);
+            $table->integer('points')->default(0);
+            $table->timestamps();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
